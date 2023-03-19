40df18fef74121d8ddcd8640ab8ae49a36ef8f86
MaxTaraev <theliron1337@gmail.com>
2023-03-19 23:17:37 +0500

    Add LoginPage object, locators and login_link_leads_to_login_page test

--- test_main_page.py
+++ test_main_page.py
@@ -7,7 +7,8 @@ from selenium.webdriver.support import expected_conditions as EC
 import os
 import time
 import math
-from Pages.main_page import MainPage
+from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
 # pytest -v --tb=line --language=en test_main_page.py
 
@@ -26,3 +27,13 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+def test_login_link_leads_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
